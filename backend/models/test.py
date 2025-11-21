94992af36f6045e1302379e86eeaf42ec32b0cde
Satyam Gupta <142325397+Sgupta-Code@users.noreply.github.com>
2025-11-21 21:13:18 +0530

    Delete backend/models directory

--- backend/models/test.py
+++
@@ -1,8 +0,0 @@
-from response_generator import ResponseGenerator
-from dotenv import load_dotenv
-load_dotenv()
-
-
-rg = ResponseGenerator()
-out = rg.generate_response("What is gradient descent in machine learning?", intent="explain")
-print(out['response'])
